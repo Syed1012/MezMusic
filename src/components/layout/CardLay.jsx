d44bef14143d57fec861ae017fc533fd814913bc
Syed1012 <syedmusaib66@gmail.com>
2024-03-05 17:03:54 +0530

    minor changes

---
+++ src/components/layout/CardLay.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Carlay.css";
+import Imga from "../../assets/indie.jpg";
+
+const CardLay = () => {
+  return (
+    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <a href="#">
+        <img className="rounded-t-lg" src={Imga} alt="Not there" />
+      </a>
+      <div className="p-5">
+        <a href="#">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Noteworthy technology acquisitions 2021
+          </h5>
+        </a>
+        <a
+          href="#"
+          className="mt-4 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Click to Play
+        </a>
+        <button>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+            <path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z" />
+          </svg>
+          clock
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CardLay;
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Carousel from "../layout/CarouselHome";
+import CardLay from "../layout/CardLay";
+
 
 const HomePage = () => {
   return (
@@ -7,91 +9,10 @@ const HomePage = () => {
       <div className="container w-full h-screen">
         <Carousel />
       </div>
-      <div className="container bg-black w-full h-screen">
-        <div className="col-12 col-md-6 mt-5">
-          {/* Profile Card */}
-          <div className="card bg-primary shadow-soft border-light mb-5 text-center">
-            <div className="profile-image shadow-inset border border-light bg-primary p-3 rounded-circle mt-n5 mx-auto">
-              <img
-                src="../../assets/img/team/profile-picture-1.jpg"
-                className="card-img-top shadow-soft p-3 border border-light rounded-circle"
-                alt="Joseph Avatar"
-              />
-            </div>
-            <div className="card-body">
-              <h3 className="h5 mb-2">Joseph Garth</h3>
-              <span className="h6 font-weight-normal text-gray mb-3">
-                Designer
-              </span>
-              <ul className="list-unstyled d-flex justify-content-center my-3">
-                <li>
-                  <a
-                    href="#"
-                    target="_blank"
-                    aria-label="facebook social link"
-                    className="icon icon-xs icon-facebook mr-3"
-                  >
-                    <span className="fab fa-facebook-f" />
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    target="_blank"
-                    aria-label="twitter social link"
-                    className="icon icon-xs icon-twitter mr-3"
-                  >
-                    <span className="fab fa-twitter" />
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    target="_blank"
-                    aria-label="slack social link"
-                    className="icon icon-xs icon-slack mr-3"
-                  >
-                    <span className="fab fa-slack-hash" />
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    target="_blank"
-                    aria-label="dribbble social link"
-                    className="icon icon-xs icon-dribbble mr-3"
-                  >
-                    <span className="fab fa-dribbble" />
-                  </a>
-                </li>
-              </ul>
-              <div className="row mt-5">
-                <div className="col-4">
-                  <div className="icon icon-shape icon-shape-xs icon-facebook icon-shape-primary shadow-soft border border-light rounded-circle mb-2">
-                    <span className="fab fa-facebook-f" />
-                  </div>
-                  <h2 className="h5 mb-0">25K</h2>
-                  <span className="small">Followers</span>
-                </div>
-                <div className="col-4">
-                  <div className="icon icon-shape icon-shape-xs icon-dribbble icon-shape-primary shadow-soft border border-light rounded-circle mb-2">
-                    <span className="fab fa-dribbble" />
-                  </div>
-                  <h2 className="h5 mb-0">5K</h2>
-                  <span className="small">Followers</span>
-                </div>
-                <div className="col-4">
-                  <div className="icon icon-shape icon-shape-xs icon-behance icon-shape-primary shadow-soft border border-light rounded-circle mb-2">
-                    <span className="fab fa-behance" />
-                  </div>
-                  <h2 className="h5 mb-0">62K</h2>
-                  <span className="small">Followers</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className="container">
+      <CardLay />
       </div>
+      
     </>
   );
 };
