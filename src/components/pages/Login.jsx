8e6df8538bb272a3f5c5bcfd7f364681d656d1fa
Syed1012 <syedmusaib66@gmail.com>
2024-03-07 08:42:10 +0530

    Regsiter button in login form

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -51,53 +51,60 @@ const Login = () => {
   // };
 
   return (
-    <div className="flex mt-6 justify-center items-center h-screen">
-      <div className="bg-white p-8 w-full sm:w-96 shadow-[0_10px_20px_rgba(240,_46,_170,_0.7)] group hover:shadow-[rgba(0,_0,_0,_0.2)_0px_60px_40px_-7px] rounded-3xl">
-        <h2 className="text-2xl font-bold mb-6">Login</h2>
-        <form className="space-y-4">
-          <div>
-            <label htmlFor="email" className="font-medium flex items-start">
-              Email
-            </label>
-            <input
-              type="text"
-              id="email"
-              placeholder="Enter Email"
-              className="mt-3 h-10 text-center block w-full rounded border-gray-300 shadow-inner shadow-purple-500/40 hover:shadow-purple-950/40 focus:border-purple-500 focus:ring focus:ring-purple-500 focus:ring-opacity-50"
-              autoComplete="off"
-            />
-          </div>
-          <div>
-            <label htmlFor="password" className="font-medium flex items-start">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              placeholder="Enter Password"
-              className="mt-3 h-10 text-center block w-full rounded border-gray-300 shadow-inner shadow-purple-500/40 hover:shadow-purple-950/40 focus:border-purple-500 focus:ring focus:ring-purple-500 focus:ring-opacity-50"
-              autoComplete="off"
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-purple-500 text-white font-bold py-2 rounded shadow-md hover:bg-purple-700"
-          >
-            Login
-          </button>
+    <>
+      <div className="flex mt-6 justify-center items-center h-screen">
+        <div className="bg-white p-8 w-full sm:w-96 shadow-[0_10px_20px_rgba(240,_46,_170,_0.7)] group hover:shadow-[rgba(0,_0,_0,_0.2)_0px_60px_40px_-7px] rounded-3xl">
+          <h2 className="text-2xl font-bold mb-6">Login</h2>
+          <form className="space-y-4">
+            <div>
+              <label htmlFor="email" className="font-medium flex items-start">
+                Email
+              </label>
+              <input
+                type="text"
+                id="email"
+                placeholder="Enter Email"
+                className="mt-3 h-10 text-center block w-full rounded border-gray-300 shadow-inner shadow-purple-500/40 hover:shadow-purple-950/40 focus:border-purple-500 focus:ring focus:ring-purple-500 focus:ring-opacity-50"
+                autoComplete="off"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="font-medium flex items-start"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Enter Password"
+                className="mt-3 h-10 text-center block w-full rounded border-gray-300 shadow-inner shadow-purple-500/40 hover:shadow-purple-950/40 focus:border-purple-500 focus:ring focus:ring-purple-500 focus:ring-opacity-50"
+                autoComplete="off"
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-purple-500 text-white font-bold py-2 rounded shadow-md hover:bg-purple-700"
+            >
+              Login
+            </button>
 
-          <div className="loweritems">
-            <span className="mt-6 text-center font-mono">
-              <Link>Forgot-Password</Link>
-            </span>
-            <p className="mt-3 font-mono">
-              Are you new here{" "}
-              <Link className="ml-3hover:underline font-mono">Click-Here</Link>
-            </p>
-          </div>
-        </form>
+            <div className="loweritems">
+              <span className="mt-6 text-center font-mono">
+                <Link>Forgot-Password</Link>
+              </span>
+              <p className="mt-3 font-mono">
+                Are you new here{" "}
+                <Link className="ml-1 hover:underline font-mono">
+                  Click-Here
+                </Link>
+              </p>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
