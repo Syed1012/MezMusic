88c1e92e9899de60fec3c6734b76d800b64edd98
Syed1012 <syedmusaib66@gmail.com>
2024-03-19 10:35:20 +0530

    Edited Carousel

--- src/components/layout/CarouselHome.jsx
+++ src/components/layout/CarouselHome.jsx
@@ -11,7 +11,13 @@ import nikImg from "../../assets/electro-music.jpg";
 const Carousel = () => {
   const [activeIndex, setActiveIndex] = useState(0);
   const [isPlaying, setIsPlaying] = useState(true);
-  const images = [logoIMg, travisImg, covImg, blackImg, nikImg];
+  const images = [
+    "https://img.wynk.in/unsafe/880x307/filters:no_upscale():strip_exif():format(webp)/https://s3.ap-south-1.amazonaws.com/discovery-prod-arsenal/arsenal/artworks/65f2c9e3155e9074bb81e238/BANNER_862765895187107.jpg",
+    blackImg,
+    "https://img.wynk.in/unsafe/880x307/filters:no_upscale():strip_exif():format(webp)/https://s3.ap-south-1.amazonaws.com/discovery-prod-arsenal/arsenal/artworks/65e9b13210ae400438d8b025/BANNER_1200258392290136.png",
+    covImg,
+    "https://img.wynk.in/unsafe/880x307/filters:no_upscale():strip_exif():format(webp)/https://s3.ap-south-1.amazonaws.com/discovery-prod-arsenal/arsenal/artworks/65f42126b41bbe152b196005/BANNER_25008486182538.jpg",
+  ];
 
   useEffect(() => {
     let intervalId;
